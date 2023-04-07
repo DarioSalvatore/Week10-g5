ca50bc688300a4974cdf2f787ca98ba76bb521ff
DarioSalvatore <dariopay69@gmail.com>
2023-04-07 17:08:30 +0200

    week10-g5

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MyHome from "./Components/MyHome";
+
+function App() {
+  return (
+    <>
+      <MyHome />
+    </>
+  );
+}
+
+export default App;

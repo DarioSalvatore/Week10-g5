684a96eb6d07c5b70cff5bbf910aee964f725bdb
DarioSalvatore <dariopay69@gmail.com>
2023-04-09 20:09:38 +0200

    bene ma non benissimo

--- src/Components/MyHome.jsx
+++ src/Components/MyHome.jsx
@@ -1,9 +1,12 @@
 import image from "../Images/fiore-di-loto-.jpg";
 import { useState, useEffect } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
 
 const MyHome = () => {
   const [found, setFound] = useState([]);
+  const [weather, setWeather] = useState([]);
+  const [prediction, setPrediction] = useState([]);
   const [content, setContent] = useState("");
   const handlechange = (e) => {
     setContent(e.target.value);
@@ -28,6 +31,7 @@ const MyHome = () => {
       const resp = await fetch(firstOne + key);
       console.log(resp);
       if (resp.ok) {
+        fiveDays();
         const data = await resp.json();
         console.log(data);
         setFound(data);
@@ -53,17 +57,33 @@ const MyHome = () => {
         "&lon=" +
         long +
         "&appid=";
-      const resp1 = await fetch(secondOne + key);
+      const resp1 = await fetch(secondOne + key + "&units=metric");
       console.log(resp1);
       if (resp1.ok) {
         const data1 = await resp1.json();
-        console.log(data1);
+
+        setWeather(data1);
+        console.log(weather);
       }
     } catch (error) {
       console.log("non è andato a buon fine!");
     }
   };
 
+  const fiveDays = async () => {
+    try {
+      const predictFetch = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=875cdbaea720a7925be5d0c4d470a196`;
+
+      const resp2 = await fetch(predictFetch + "&units=metric");
+      if (resp2.ok) {
+        const data2 = await resp2.json();
+        setPrediction(data2);
+      }
+    } catch (err) {
+      console.log("hai fallito!");
+    }
+  };
+  console.log("hai trovato", prediction);
   return (
     <div id="sfondo">
       <div className="d-flex position-relative">
@@ -76,27 +96,30 @@ const MyHome = () => {
               value={content}
               onChange={handlechange}
             />
-            <Button variant="success" type="submit">
-              {" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-search"
-                viewBox="0 0 16 16"
-              >
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-              </svg>
-            </Button>
           </Form>
         </div>
         <div className="position-absolute d-flex flex-column hero align-items-center">
-          <p className="fw-bold">Soleggiato</p>
-          <h3 className="fw-bold">23°</h3>
-          <p className="fw-bold">Berlino</p>
+          {weather.name ? <h3 className="fw-bold">{weather.name}</h3> : null}
+          {weather.main ? (
+            <p className="fw-bold">{weather.main.temp}&#176;C</p>
+          ) : null}
+          {weather.weather ? (
+            <p className="fw-bold">{weather.weather[0].description}</p>
+          ) : null}
         </div>
-        <div className="position-absolute d-flex hero align-items-center"></div>
+        <div>
+          {/* {prediction[0].list.slice(1, 5).map((day, i) => (
+            <Card key={i.dt} data={i} style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          ))} */}
+        </div>
       </div>
     </div>
   );
